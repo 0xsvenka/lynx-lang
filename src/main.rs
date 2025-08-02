ebeb8ef647e25c46f42fe3fc6bb54ee595bad754
0xsvenka <svenka@tutamail.com>
2025-08-02 16:00:44 +0800

    Modify token.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod token;
+
+use logos::Logos;
+use token::Token;
+
 fn main() {
-    println!("Hello, Lynx!");
+    let src = r#"
+    for i in range do (
+        println(i)
+        if math.is_prime(i) then True
+        else False
+    )
+    "#;
+
+    let lex = Token::lexer(src);
+
+    for result in lex {
+        match result {
+            Ok(token) => println!("{:#?}", token),
+            Err(_) => panic!("Syntax Error"),
+        }
+    }
 }
